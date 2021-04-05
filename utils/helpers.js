269669c05e97fb7269766ac64ca949c98d0e71fd
hadeediqbal <hadeediqbal@yahoo.com>
2021-04-04 21:22:25 -0500

    project folders

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,33 +1,26 @@
 module.exports = {
   format_date: (date) => {
-    if (new Date(date).getHours() > 12) {
-      return `${new Date(date).getMonth() + 1}/${new Date(
-        date
-      ).getDate()}/${new Date(date).getFullYear()} at: ${new Date(
-        date
-      ).getHours()}:${("0" + new Date(date).getMinutes()).slice(-2)} PM`;
-    } else {
-      return `${new Date(date).getMonth() + 1}/${new Date(
-        date
-      ).getDate()}/${new Date(date).getFullYear()} at: ${new Date(
-        date
-      ).getHours()}:${("0" + new Date(date).getMinutes()).slice(-2)} AM`;
-    }
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
   },
+  // comment_time: (date) => {
+  //   if (new Date(date).getHours() > 12) {
+  //     return `${new Date(
+  //       date
+  //     ).getHours()-12}:${("0" + new Date(date).getMinutes()).slice(-2)} PM`;
+  //   } else {
+  //     return `${new Date(
+  //       date
+  //     ).getHours()}:${("0" + new Date(date).getMinutes()).slice(-2)} AM`;
+  //   }
+  // },
   format_plural: (word, amount) => {
     if (amount !== 1) {
       return `${word}s`;
     }
 
     return word;
-  },
-  format_url: (url) => {
-    return url
-      .replace("http://", "")
-      .replace("https://", "")
-      .replace("www.", "")
-      .split("/")[0]
-      .split("?")[0];
   },
   format_time: (time) => {
     // Check correct time format and split into components
@@ -42,8 +35,5 @@ module.exports = {
       time[0] = +time[0] % 12 || 12; // Adjust hours
     }
     return time.join(""); // return adjusted time or original string
-  },
-  sort_comments: (comments) => {
-    return comments.sort();
   },
 };
